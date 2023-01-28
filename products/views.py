dae6043d4fb34422fbfb2996809260fab6deebf6
yazan <yazanzeidany95@gmail.com>
2023-01-29 01:19:49 +0300

    add urls to the projects and pront helloworld to the products app

--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse('Hellow World')
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
 
 
+def new(request):
+    return HttpResponse('New Products')
